96d42fe1358f28b18635074fa64150ebf3bd4709
Safir <zhaocongnian@126.com>
2023-03-22 09:43:31 +0800

    remove x265

--- wasm/build-scripts/install-deps.sh
+++ wasm/build-scripts/install-deps.sh
@@ -15,7 +15,12 @@ done
 if [ ${#cmds[@]} -ne 0 ];
 then
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    apt-get update
+  echo "" > /etc/apt/sources.list  \
+    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free" >> /etc/apt/sources.list  \
+    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-updates main contrib non-free" >> /etc/apt/sources.list  \
+    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-backports main contrib non-free" >> /etc/apt/sources.list  \
+    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list \
+    && apt update
     apt-get install -y ${cmds[@]}
   else
     brew install ${cmds[@]}
