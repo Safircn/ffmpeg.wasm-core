c38b7ea305d596e6c8e3d04271158afa2d561adb
Safir <zhaocongnian@126.com>
2023-03-22 13:39:29 +0800

    切换apt源

--- wasm/build-scripts/install-deps.sh
+++ wasm/build-scripts/install-deps.sh
@@ -15,12 +15,13 @@ done
 if [ ${#cmds[@]} -ne 0 ];
 then
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  echo "" > /etc/apt/sources.list  \
-    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free" >> /etc/apt/sources.list  \
-    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-updates main contrib non-free" >> /etc/apt/sources.list  \
-    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-backports main contrib non-free" >> /etc/apt/sources.list  \
-    && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list \
-    && apt update
+  # echo "" > /etc/apt/sources.list  \
+  #   && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free" >> /etc/apt/sources.list  \
+  #   && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-updates main contrib non-free" >> /etc/apt/sources.list  \
+  #   && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-backports main contrib non-free" >> /etc/apt/sources.list  \
+  #   && echo "deb https://mirrors.tuna.tsinghua.edu.cn/debian-security bullseye-security main contrib non-free" >> /etc/apt/sources.list \
+  #   && apt update
+    apt-get update
     apt-get install -y ${cmds[@]}
   else
     brew install ${cmds[@]}
